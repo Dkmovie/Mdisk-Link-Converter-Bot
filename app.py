8f15698a73170be4eea78e10aba6e9dc1b3e252d
Testdk2 <120616619+Testdk2@users.noreply.github.com>
2022-12-15 13:41:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
+
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
+os.system("git clone https://Testdk2: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@github.com/Testdk2/Mdisk-Link-Converter-Bot okk && cd okk && pip3 install -U -r requirements.txt && gunicorn app:app && python3 main.py &")
