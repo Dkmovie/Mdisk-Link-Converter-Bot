366397dfa56d18e209c8bbb473cf0a4af268b522
Testdk2 <120616619+Testdk2@users.noreply.github.com>
2022-12-15 13:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Testdk2: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@github.com/Testdk2/Mdisk-Link-Converter-Bot okk && cd okk && pip3 install -U -r requirements.txt && gunicorn app:app && python3 main.py &")
+os.system("git clone https://Testdk2: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@github.com/Testdk2/Mdisk-Link-Converter-Bot okk && cd okk && pip3 install -U -r requirements.txt && python3 bot.py &")
